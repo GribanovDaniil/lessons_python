8ec2face4d1d1b53214a68650614d1d3122ebc18
GribanovDaniil <113581434+GribanovDaniil@users.noreply.github.com>
2022-10-31 19:59:37 +0300

    Delete zada4i 12.09.2022 directory

--- zada4i 12.09.2022/z3.py
+++
@@ -1,9 +0,0 @@
-first = int(input('К оплате:'))
-second = int(input('К оплате:'))
-third = int(input('К оплате:'))
-if first > second and second > third:
-    print ('Акция!',sum([first, second, third])/3)
-elif first < second and second < third:
-    print ('Акция!', sum([first, second, third])/2)
-else:
-    print('К оплате:')
