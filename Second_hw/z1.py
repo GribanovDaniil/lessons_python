9dffef45c4ad6b309d4eb2db9f9c44955e530006
GribanovDaniil <113581434+GribanovDaniil@users.noreply.github.com>
2022-11-23 22:09:16 +0300

    Delete Second_hw directory

--- Second_hw/z1.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def countSquares(self, matrix):
-        for i in range(1, len(matrix)):
-            for j in range(1, len(matrix[0])):
-                matrix[i][j] *= min(matrix[i - 1][j], matrix[i][j - 1], matrix[i - 1][j - 1]) + 1
-        return sum(map(sum, matrix))
-
-#сложность O(n**2)
-
